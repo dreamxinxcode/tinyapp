52cd677c466437cf83284b46be2d785f94fd3c21
Brandon Lecky <dreamxinxcode@gmail.com>
2020-08-03 23:41:40 +0000

    Appears to work

--- express_server.js
+++ express_server.js
@@ -17,27 +17,33 @@ const urlDatabase = {
 const generateRandomString = () => {
   let str = '';
   const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  for (let i = 0; i <= 5; i++) {
+  for (let i = 0; i < 6; i++) {
     str += chars.charAt(Math.floor(Math.random() * chars.length));
   }
   return str;
 };
 
 app.get('/', (req, res) => {
-  res.send('Hello!');
+  const templateVars = {
+    urls: urlDatabase,
+  };
+  res.render('urls_index', templateVars);
 });
 
 app.get('/urls', (req, res) => {
   const templateVars = {
     urls: urlDatabase,
-    hello: 'hello world'
   };
   res.render('urls_index', templateVars);
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  let key = generateRandomString();
+  urlDatabase[key] = req.body.longURL;
+  const templateVars = {
+    urls: urlDatabase,
+  };
+  res.render('urls_index', templateVars);
 });
 
 app.get('/urls/new', (req, res) => {
@@ -50,6 +56,11 @@ app.get("/urls/:shortURL", (req, res) => {
     longURL: req.params.longURL
   };
   res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
 app.get('/urls.json', (req, res) => {
